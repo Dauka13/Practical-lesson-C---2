e344f2d21e1a55fa96c41945193d3c146ef24c0f
Dauka <orazbekovdd13@mail.ru>
2022-08-15 19:39:02 +0600

    add task 008 numberscomposition

---
+++ Task_008/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+string writeLetterN = Console.ReadLine();
+int N = int.Parse(writeLetterN);
+
+int NumbersComposition(int x)
+{
+    int comp = 1;
+    for (int num = 1; num <= x; num++)
+    {
+        comp = comp * num;
+    }
+    return comp;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {NumbersComposition(N)}");
