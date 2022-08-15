89b4d4b0e13c9c6b8d0df55850e0732e32b47785
Dauka <orazbekovdd13@mail.ru>
2022-08-15 18:09:20 +0600

    add task 006 about quantity of numbers

---
+++ Task_006/Program.cs
@@ -0,0 +1,16 @@
+﻿// Определить количество цифр в числе
+
+Console.Clear();
+Console.Write("Введите число: ");
+string writeNumber = Console.ReadLine();
+int number = int.Parse(writeNumber);
+int result = number;
+
+int count1 = 0;
+while (result > 0)
+{
+    result = result / 10;
+    count1++;
+}
+Console.WriteLine($"В числе {number} {count1} цифр");
+Console.WriteLine();
