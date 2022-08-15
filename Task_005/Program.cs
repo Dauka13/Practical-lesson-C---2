31277ae2a13d305f0668572c39775906edcd2066
Dauka <orazbekovdd13@mail.ru>
2022-08-15 17:58:50 +0600

    add task 005 sum numbers

---
+++ Task_005/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.Clear();
+Console.Write("Введите число: ");
+string numberA = Console.ReadLine();
+int A = int.Parse(numberA);
+
+int SumNumber(int A)
+{
+    int number = 1;
+    int sum = 0;
+    for (int i = 0; i < A; i++)
+    {
+        sum = sum + number;
+        number++;
+    }
+    return sum;
+}
+
+int NumbersSum = SumNumber(A);
+Console.WriteLine($"Сумма чисел от 1 до {A} составляет {NumbersSum}");
