89a3de50b8b269f94fe5b09ba9f308254d18c389
Dauka <orazbekovdd13@mail.ru>
2022-08-15 17:21:02 +0600

    add task 003 about possibles quarter's

---
+++ Task_003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Clear();
+int quarter = new Random().Next(1, 5);
+string range = "";
+string num = "";
+
+if (quarter == 1) range = "x > 0, y > 0";
+else if (quarter == 2) range = "x < 0, y > ";
+else if (quarter == 3) range = "x < 0, y < 0";
+else range = "x > 0, y < 0";
+
+Console.WriteLine($"Для {quarter}-й четверти координаты точки должны быть заданы в диапазоне ( {range} )");
