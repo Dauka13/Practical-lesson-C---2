a1b55838063ef81b4befea1fa8ee7dad1e52f6fe
Dauka <orazbekovdd13@mail.ru>
2022-08-15 19:19:07 +0600

    add task 007 about sumDigit

---
+++ Task_007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+int number = new Random().Next(1, 100000);
+int sumDigit = 0;
+Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
+}
+
+Console.WriteLine(sumDigit);
